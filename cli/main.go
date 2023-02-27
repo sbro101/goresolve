d41564084f289b93777ead805dd822e5d53636f3
Sebastian Broekhoven <2205121+sbroekhoven@users.noreply.github.com>
2023-02-27 22:26:07 +0100

    testing

---
+++ cli/main.go
@@ -0,0 +1,23 @@
+package main
+
+// this is to test if the module is working
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/sbroekhoven/goresolve"
+)
+
+func main() {
+	url := os.Args[1]
+
+	rd := goresolve.Hostname(url, "1.1.1.1")
+
+	json, err := json.MarshalIndent(rd, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", json)
+}
